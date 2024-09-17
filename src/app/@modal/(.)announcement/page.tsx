e8ef8ba8450a78cf37ad8423f3d6b5d5e242f733
Lyle Liao <lyle.stack.life@gmail.com>
2024-09-17 23:54:32 +0800

    add intercepting route for modal

---
+++ src/app/@modal/(.)announcement/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Modal from "@/components/Modal";
+import AnnouncementContent from "@/app/(components)/AnnouncementContent";
+
+export default function Announcement() {
+  return (
+    <Modal>
+      <AnnouncementContent />
+    </Modal>
+  );
+}
