12f055366ccac12f0882411ef46f150c9edae2ae
Lyle Liao <lyle.stack.life@gmail.com>
2024-09-25 20:55:29 +0800

    update: sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -8,5 +8,29 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "monthly",
       priority: 1,
     },
+    {
+      url: "https://lyleliao.com/v2",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://lyleliao.com/announcement",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.64,
+    },
+    {
+      url: "https://lyleliao.com/v2/timeline",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.64,
+    },
+    {
+      url: "https://lyleliao.com/v2/resume",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.64,
+    },
   ];
 }
